4281416133e3cc62b3f471090fbc62200b83591d
Mariana <mbravo@thoughtworks.com>
2014-08-29 15:58:20 -0500

    Improving specs for schedule serializer

--- spec/serializers/schedule_serializer_spec.rb
+++ spec/serializers/schedule_serializer_spec.rb
@@ -1,10 +1,18 @@
 describe ScheduleSerializer do
   before :each do
-    @schedule = FactoryGirl.create :schedule
+    @schedule = FactoryGirl.build :schedule, id: BSON::ObjectId.new
     @json = ScheduleSerializer.new(@schedule, root: false).to_json
   end
 
   it 'should contain id as string' do
-    expect(@json).to match(/"id":"#{@schedule.id.to_s}"/)
+    expect(@json).to match(/"id":"#{@schedule.id}"/)
+  end
+
+  it 'should contain name as string' do
+    expect(@json).to match(/"name":"#{@schedule.name}"/)
+  end
+
+  it 'should contain times as array of strings' do
+    expect(@json).to match(/"times":\[("\d\d:\d\d",?)+\]/)
   end
 end
